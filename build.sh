6674aa05f63df3389f5de29bab4ddc8f31a31535
YunJae Lee <gkfthddk@gmail.com>
2021-09-24 16:27:19 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ mkdir box
 cp ../DRsim/run_ele.mac DRsim/
 cp ../DRsim/run_pi.mac DRsim/
 cp ../DRsim/run_en.mac DRsim/
+cp ../DRsim/run_gps.mac DRsim/
 cmake ..
 make -j8
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HEPMC_DIR/lib64:$FASTJET_DIR/lib:$PYTHIA_DIR/lib:$PWD/lib
@@ -14,10 +15,16 @@ cp rootIO/{librootIO_rdict.pcm,librootIO.rootmap} lib/
 cp -r ../Detector/DRcalo/compact bin/
 mkdir -p DRsim/bin
 mkdir -p Reco/bin
+mkdir -p analysis/bin
 cp -r ../Detector/DRcalo/compact Reco/bin/
 cp -r ../Detector/DRcalo/compact DRsim/bin/
+cp -r ../Detector/DRcalo/compact analysis/bin/
 
+cp bin/P8 Gen/
 cp bin/DRsim DRsim/
 cp bin/Reco Reco/
+cp bin/process analysis/
 
-#./DRsim run_ele.mac 0 testel
+cd ..
+unset PYTHONPATH
+unset PYTHONHOME
